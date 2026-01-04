878b929d89e0eccc9a157c707ab8a00d9289cc89
marcusneuhaus76-create <marcusneuhaus76@gmail.com>
2026-01-04 22:57:39 +0100

    05 and 06 JSON in JS - Array in script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let questions  = [
+    {
+        "question": "Wer hat HTML erfunden",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    }
+];
+
+
+// https://jsonformatter.curiousconcept.com/
+// ggf. noch weitere JSON-Elemente überlegen
