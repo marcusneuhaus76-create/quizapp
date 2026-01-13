6b7bb942579b96f49e24a72d1a3c2adadb13f907
marcusneuhaus76-create <marcusneuhaus76@gmail.com>
2026-01-14 00:25:24 +0100

    Own Attempt to end the quizz-Site

--- script.js
+++ script.js
@@ -63,10 +63,13 @@ let questions  = [
 
 let currentQuestion = 0;
 
+let rightAnswers = 0;
+
 function init() {
-      document.getElementById('questionSum').innerHTML = questions.length;
+    mainfunction();
+      document.getElementById('questionSum').innerHTML = questions.length;      
       showquestion();
-      document.getElementById('currentQuestionNumber').innerHTML = currentQuestion + 1;
+    //   document.getElementById('currentQuestionNumber').innerHTML = currentQuestion + 1;
 }
 
 function showquestion() {
@@ -88,9 +91,12 @@ function showquestion() {
 
 function answer(selection){                    /* 'selection' ist der übergebene Parameter, wenn der User eine Antwort anklickt  */
     let question = questions[currentQuestion];  /* Das 1. Position des Arrays (Stelle '0') wid in der Variablen 'quetion' gespeichert   */
+   
     console.log('Selected answer is    ', selection);  // Gibt den Inhalt der Variable 'selection' in der Konsole aus. Inhaltvon 'selection' ist der angeklickte Button (z.B. 'answer_1')
     console.log('Current answer is    ', question); // Gibt den Inhalt der Variable 'question' in der Konsole aus. Inhalt von 'question' ist das aktuelle Fragen-Objekt aus dem Array 'questions'
+    
     let selectedQuestionNumber = selection.slice(-1); // Variable speichert die letzte Zeichen des Strings 'selection' in der Variable 'selectedQuestionNumber'. Z.B. aus 'answer_1' wird '1'
+    
     console.log('selectedQuestionNumber ', selectedQuestionNumber); // Variable 'selectedQuestionNumber' wird in der Konsole ausgegeben
     console.log('Current answer is    ', question['right_answer']); // Gibt die richtige Antwort der aktuellen Frage in der Konsole aus. Z.B. '3' für die erste Frage
 
@@ -100,28 +106,46 @@ function answer(selection){                    /* 'selection' ist der übergeben
 
     if(selectedQuestionNumber == question['right_answer']) {  /* Vergleicht die ausgewählte Antwort mit der richtigen Antwort */
         console.log('Richtige Antwort!!');  /* Wenn die Antworten übereinstimmen, wird 'Richtige Antwort' in der Konsole ausgegeben */
-        document.getElementById(selection).parentNode.classList.add('bg-success'); /* Der angeklickte Button wird grün hinterlegt */       
+        document.getElementById(selection).parentNode.classList.add('bg-success'); /* Der angeklickte Button wird grün hinterlegt */    
+        rightAnswers++; /* Die Anzahl der richtigen Antworten wird um 1 erhöht */
+        console.log('Richtige Antworten insgesamt: ', rightAnswers); /* Gibt die Gesamtzahl der richtigen Antworten in der Konsole aus */   
     } else {
         console.log('Falsche Antwort');
         document.getElementById(selection).parentNode.classList.add('bg-danger'); 
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success'); /* Der angeklickte Button wird rot hinterlegt */
     }
-    document.getElementById('next-button').disabled = false; /* Der 'Nächste Frage' Button wird aktivier */
+    document.getElementById('next-button').disabled = false; /* Der 'Nächste Frage' Button wird aktiviert */
 }
 
 function nextQuestion() {
-    currentQuestion++;
-    showquestion();
+     currentQuestion++;
+     
 
-    document.getElementById('next-button').disabled = true;
-    selection = null;
+    if(currentQuestion >= questions.length) {
+        endFunction();
+        return;
 
-    document.getElementById('answer_1').parentNode.classList.remove('bg-success','bg-danger');
-    document.getElementById('answer_2').parentNode.classList.remove('bg-success','bg-danger');
-    document.getElementById('answer_3').parentNode.classList.remove('bg-success','bg-danger');
-    document.getElementById('answer_4').parentNode.classList.remove('bg-success','bg-danger');
+    } else {
 
-    document.getElementById('currentQuestionNumber').innerHTML = currentQuestion + 1;
+        showquestion();
+
+        document.getElementById('next-button').disabled = true;
+        selection = null;
+
+        document.getElementById('answer_1').parentNode.classList.remove('bg-success','bg-danger');
+        document.getElementById('answer_2').parentNode.classList.remove('bg-success','bg-danger');
+        document.getElementById('answer_3').parentNode.classList.remove('bg-success','bg-danger');
+        document.getElementById('answer_4').parentNode.classList.remove('bg-success','bg-danger');
+
+        document.getElementById('currentQuestionNumber').innerHTML = currentQuestion + 1;
+        // document.getElementById('end-screen').style = '';
+
+
+       /*  document.getElementById('question-screen').style = 'display: none;';
+        document.getElementById('amount-of-questions').innerHTML = questions.length;
+        document.getElementById('right-answers').innerHTML = rightAnswers;
+        document.getElementById('header-image').src = 'img/trophy.png'; */
+    }
 }
 
 /* My Proposal  to handle answer selection 
@@ -136,3 +160,62 @@ function answer(selection) {
 
 
 // 11 - Was passiert bei einer falschen Antwort
+
+// Für 16: Quiz Ende und Ergebnis anzeigen oder Formulieren wie der letzte Screen aussehen soll. Siehe letzten Screenshot. 
+
+function mainfunction() {
+    
+    document.getElementById('question-screen').innerHTML= `
+                                                    <div class="card quiz-card" style="width: 18rem;">
+                                                                    <img src ="./img/pencil-5800079_1280.jpg" class="card-img-top" alt="...">
+                                                                    <div class="card-body">
+                                                                        <h5 class="card-title" id="questiontext">Frage</h5>
+                                                                        
+                                                                        <div class="card quiz-answer-card mb-2" onclick="answer('answer_1')">
+                                                                            <div class="card-body" id="answer_1">
+                                                                                Antwort
+                                                                            </div>
+                                                                        </div>
+
+                                                                        <div class="card quiz-answer-card mb-2" onclick="answer('answer_2')">
+                                                                            <div class="card-body" id="answer_2">
+                                                                                Antwort
+                                                                            </div>
+                                                                        </div>
+
+                                                                        <div class="card quiz-answer-card mb-2" onclick="answer('answer_3')">
+                                                                            <div class="card-body" id="answer_3">
+                                                                                Antwort
+                                                                            </div>
+                                                                        </div>
+
+                                                                        <div class="card quiz-answer-card mb-2" onclick="answer('answer_4')">
+                                                                            <div class="card-body" id="answer_4">
+                                                                                Antwort
+                                                                            </div>
+                                                                        </div>
+
+                                                                        <div class="question-footer">
+                                                                            <span>  
+                                                                                <b id="currentQuestionNumber">1</b> von <b id="questionSum"></b> Fragen
+                                                                            </span>    
+                                                                                
+                                                                            <button onclick="nextQuestion()" class="btn btn-primary" id="next-button" disabled>Nächste Frage</button>
+                                                                        </div>
+                                                                                                
+                                                                    </div>
+                                                                </div>
+                                                                        `;
+}
+
+function endFunction() {   
+     console.log('Quiz beendet');
+     document.getElementById('question-screen').innerHTML= `<div class="card quiz-card" style="width: 18rem;">
+                                                                    <img src ="./img/pencil-5800079_1280.jpg" class="card-img-top" alt="...">
+                                                                    <div class="card-body">
+                                                                        <h5 class="card-title">Quiz beendet</h5>
+                                                                        <p class="card-text">Herzlichen Glückwunsch! Sie haben das Quiz erfolgreich abgeschlossen.</p>
+                                                                        <button onclick="restartQuiz()" class="btn btn-primary">Neustart</button>
+                                                                    </div>
+                                                                </div>`;
+}
